1c926280c508ebe7d6b160ad059e7208e28aa6a8
krunal <kjethi@officebrain.com>
2017-10-31 15:58:39 +0530

    compare,wishlist,QuickQuoteList

---
+++ src/services/transaction/transaction.class.js
@@ -0,0 +1,160 @@
+var rethinkCon = require('./rethinkCon.js');
+const fs = require('fs');
+
+var dbConfig={
+		host:'localhost',
+		port: 28015,
+		dbname:'feathers_demo',
+		table_name:'tbl_transaction'
+	}
+
+var successMsgArray={
+	1 : 'successFully added in wishList',
+	2 : 'successFully added in compareList',
+	3 : 'successFully added in QuickQuoteList'
+}
+var deleteMsgArray={
+	1 : 'successFully deleted in wishList',
+	2 : 'successFully deleted in compareList',
+	3 : 'successFully deleted in QuickQuoteList'
+}
+
+var dbObj = new rethinkCon(dbConfig);
+
+var responseArr={};
+/* eslint-disable no-unused-vars */
+
+class Service {
+  constructor (options) {
+    this.options = options || {};
+  }
+
+  find (params) {
+		console.log(params)
+    return new Promise(function(resolve, reject) {
+		  dbObj.getAllRecordsByUserId(params.query,function(err,result){
+			  if(err){
+				  responseArr.status='error';
+				  responseArr.error_msg=result;
+				  console.log('error')
+				  console.log(result)
+				  resolve(responseArr)
+			  }else{
+				  console.log(result)
+				  resolve(result)
+				  //return Promise.resolve("success")
+			  }
+			  
+		  })
+	  })  
+  }
+
+  get (id, params) {
+    return new Promise(function(resolve, reject) {
+		  dbObj.getAllRecordsByUserId(id,function(err,result){
+			  if(err){
+				  responseArr.status='error';
+				  responseArr.error_msg=result;
+				  console.log('error')
+				  console.log(result)
+				  resolve(responseArr)
+			  }else{
+				  console.log(result)
+				  resolve(result)
+				  //return Promise.resolve("success")
+			  }
+			  
+		  })
+	  })
+  }
+
+  create (data, params) {
+		var responseArr={};
+		data.createAt = new Date();
+
+	  return new Promise(function(resolve, reject) {
+		  dbObj.insertRecord(data,function(err,result){
+			
+			  if(err){
+				  responseArr.status='error';
+				  responseArr.error_msg=result;
+				  console.log('error')
+				  console.log(result)
+				  resolve(responseArr)
+			  }else{
+				  responseArr.status='success';
+				  responseArr.success_msg=successMsgArray[data.type]
+				  console.log('success')
+				  resolve(responseArr)
+				  //return Promise.resolve("success")
+			  }
+			  
+		  })
+	  })  
+//	  let error;
+//	  dbObj.insertRecord(data,dbConfig,function(err,result){
+//		  if(err){
+//			  error = await 
+//		  }
+//	  })
+  }
+
+  update (id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  patch (id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  remove (id, params) {
+	  var responseArr={};
+	console.log(id);
+	  
+	  return new Promise(function(resolve, reject) {
+		  dbObj.deleteRecord(id,params.query,function(err,result){
+			  if(err){
+				  responseArr.status='error';
+				  responseArr.error_msg=result;
+				  console.log('error')
+				  console.log(result)
+				  resolve(responseArr)
+			  }else{
+				  console.log('-------------------------')
+				  console.log(result)
+				  console.log('-------------------------')
+				  responseArr.status='success';
+				  responseArr.success_msg=deleteMsgArray[params.type];
+				  console.log('success')
+				  resolve(responseArr)
+				  //return Promise.resolve("success")
+			  }
+			  
+		  })
+	  })  
+//	  let error;
+//	  dbObj.insertRecord(data,dbConfig,function(err,result){
+//		  if(err){
+//			  error = await 
+//		  }
+//	  })
+  }
+  
+//  someFunction(data){
+//	  return "anything"
+//  }
+}
+
+module.exports = function (options) {
+  return new Service(options);
+};
+
+module.exports.Service = Service;
+
+function writeFile(data){
+	try{
+	    fs.writeFileSync('temp/', Data);
+	}catch (e){
+	    console.log("Cannot write file ", e);
+	}
+}
--- src/services/transaction/transaction.service.js
+++ src/services/transaction/transaction.service.js
@@ -1,16 +1,14 @@
 // Initializes the `transaction` service on path `/transaction`
-const createService = require('feathers-rethinkdb');
+const createService = require('./transaction.class.js');
 const hooks = require('./transaction.hooks');
 const filters = require('./transaction.filters');
 
 module.exports = function () {
   const app = this;
-  const Model = app.get('rethinkdbClient');
   const paginate = app.get('paginate');
 
   const options = {
     name: 'transaction',
-    Model,
     paginate
   };
 
