914fee6e722fbd2350d0d2d96ab05f3fd4f50b4c
avasani <avasani@officebrain.com>
2017-11-29 19:48:37 +0530

    shoppingCart service added and configured for accepting env variable for RDB host port

--- src/services/index.js
+++ src/services/index.js
@@ -6,6 +6,7 @@ const metalsmith = require('./metalsmith/metalsmith.service.js');
 const transaction = require('./transaction/transaction.service.js');
 const imageUpload = require('./image-upload/image-upload.service.js');
 const getDirectoryList = require('./get-directory-list/get-directory-list.service.js');
+const shoppingCart = require('./shoppingCart/shoppingCart.service.js');
 
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
@@ -19,5 +20,6 @@ module.exports = function () {
 
   app.configure(imageUpload);
   app.configure(getDirectoryList);
+  app.configure(shoppingCart);
   
 };
