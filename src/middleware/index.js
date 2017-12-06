7bde5ca3cd86de5aa6b84397c9629dfbe2301353
harshp <harshp@officebrain.com>
2017-12-06 13:00:17 +0530

    Initial commit

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,7 @@
+const subscription = require('/home/software/HarshPatel/project/subscription/index')
 module.exports = function () {
   // Add your custom middleware here. Remember, that
   // in Express the order matters
   const app = this; // eslint-disable-line no-unused-vars
+  app.use(subscription.subscription)
 };
