1ae3a86459176651895ec14bcb7c4661f7c73712
dprajpati <dprajapati@officebrain.com>
2018-01-13 10:22:39 +0530

    change public path

--- plugins/WebsiteTemplates/web2/public/assets/metalsmith.js
+++ plugins/WebsiteTemplates/web2/public/assets/metalsmith.js
@@ -2,19 +2,16 @@ var Metalsmith=require('/var/www/html/node_modules/metalsmith');
 var markdown=require('/var/www/html/node_modules/metalsmith-markdown');
 var layouts=require('/var/www/html/node_modules/metalsmith-layouts');
 var permalinks=require('/var/www/html/node_modules/metalsmith-permalinks');
-var inPlace = require('/var/www/html/node_modules/metalsmith-in-place')
+var inPlace = require('/var/www/html/node_modules/metalsmith-in-place');
 var fs=require('/var/www/html/node_modules/file-system');
 var Handlebars=require('/var/www/html/node_modules/handlebars');
-Handlebars.registerPartial('Header_header', fs.readFileSync('/var/www/html/websites/59a8e0dd41dc17001aeb1e67/aakron/temp/Header_header.html').toString())
-Handlebars.registerPartial('Footer_footer', fs.readFileSync('/var/www/html/websites/59a8e0dd41dc17001aeb1e67/aakron/temp/Footer_footer.html').toString())
-Handlebars.registerPartial('wishlist_list', fs.readFileSync('/var/www/html/websites/59a8e0dd41dc17001aeb1e67/aakron/temp/wishlist_list.html').toString())
  Metalsmith(__dirname)
 .metadata({
 title: "Demo Title",
 description: "Some Description",
 generator: "Metalsmith",
 url: "http://www.metalsmith.io/"})
-.source('/var/www/html/websites/59a8e0dd41dc17001aeb1e67/aakron/Preview')
+.source('')
 .destination('/var/www/html/websites/59a8e0dd41dc17001aeb1e67/aakron/public')
 .clean(false)
 .use(markdown())
