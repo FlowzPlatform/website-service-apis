ded5255c1c1d7b2bec22ce91216bbf4cf70fa854
Software <neelpatel@officebrain.com>
2018-02-08 10:34:01 +0530

    admin-orders and cart api changes

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,46 @@
+const handler = require('feathers-errors/handler');
+const notFound = require('feathers-errors/not-found');
+const subscription = require('flowz-subscription')
+module.exports.subscription = subscription
+
 module.exports = function () {
   // Add your custom middleware here. Remember, that
-  // in Express the order matters
-  const app = this; // eslint-disable-line no-unused-vars
+  // in Express the order matters, `notFound` and
+  // the error handler have to go last.
+  const app = this;
+
+  
+
+  
+  let totalSettings = 6
+  subscription.moduleResource.moduleName = 'website'
+  let registerAppModule = {
+    'admin-orders': ['create' , 'get' , 'update' , 'remove']
+  }
+  subscription.moduleResource.registerAppModule = registerAppModule 
+  subscription.moduleResource.appRoles = ['admin']
+  subscription.registeredAppModulesRole()
+  subscription.registerDynamicHooks(app, registerAppModule)
+  
+  //console.log(app)
+  // app.service("settings").get("cfe90fca-6a5e-4c6a-8430-a1538eeb130b").then(count => {
+  //   console.log('users:', count);
+  // }).catch(err => {
+  //     console.log(err)
+  // });
+  // subscription.secureService.validate = (route, params, secureRouteInfo , userDetail) => {
+    
+  //   return new Promise((resolve, reject) => {
+  //     // console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  //     // console.log(secureRouteInfo.value, '====', totalSettings)
+  //     if ((route === '/settings' || route === 'settings') && secureRouteInfo.value >= totalSettings) {
+  //       resolve(true)
+  //       console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>" ,1)
+  //     }else{
+  //       resolve(false)
+  //       console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>" ,2)
+  //     }
+      
+  //   })
+  // }
 };
--- src/services/index.js
+++ src/services/index.js
@@ -28,6 +28,8 @@ const myOrders = require('./my-orders/my-orders.service.js');
 
 const shippingEstimator = require('./shipping-estimator/shipping-estimator.service.js');
 
+const adminOrders = require('./admin-orders/admin-orders.service.js');
+
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(flowsDirListing);
@@ -59,4 +61,5 @@ module.exports = function () {
   app.configure(cloneWebsite);
   app.configure(saveMenu);
 
+  app.configure(adminOrders);
 };
