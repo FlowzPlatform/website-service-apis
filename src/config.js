7cf3af290d0df9a8734d4748f3a9d884e21c6b3d
mohit5540 <33310921+mohit5540@users.noreply.github.com>
2018-02-16 12:41:45 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,8 +4,6 @@ module.exports = {
     basePath: '/var/www/html/',
     gitLabUrl: 'http://47.88.50.66',
     gitLabUsername: 'fsaiyed',
-    gitLabToken: 'zfgkxJyVyhdWD35isENX',
-    mailSendApi: 'http://api.flowzcluster.tk/sendmail/email/send'
-    // gitLabToken: 'eQ1-V9hyB9PN_XYnYfkV'
+    mailSendApi: 'http://api.flowzcluster.tk/sendmail/email/send',
     gitLabToken: 'kR69TpDzkUhqE-u_s-ax'
 }
