1d5332c77502ffcbdf29c485642ade7e677cba97
Software <neelpatel@officebrain.com>
2018-02-20 16:36:26 +0530

    subscription changes for website

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -5,12 +5,14 @@ module.exports = async function () {
   subscription.moduleResource.moduleName = 'webbuilder'
   let registerAppModule = {
     'project-configuration': ['create'],
-    'delete-service': ['remove']
+    'delete-service': ['remove'],
+    'address-book': ['create', 'get', 'find', 'patch'],
+    'request-info': ['create', 'get', 'find'],
+    'myOrders': ['create', 'get', 'find'],
+    'request-quote': ['create', 'get', 'find']
   }
   subscription.moduleResource.registerAppModule = registerAppModule
   subscription.moduleResource.appRoles = ['admin', 'developer', 'client']
   subscription.registeredAppModulesRole()
-  console.log("registerAppModule.....from middle", registerAppModule)
   subscription.registerDynamicHooks(app, registerAppModule)
 };
-
