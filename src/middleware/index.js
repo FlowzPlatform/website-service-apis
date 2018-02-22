1b4e0777e45d7d2d71541fe0ba26df645b00cad7
gkumar <gkumar@officebrain.com>
2018-02-22 13:00:32 +0530

    changed middleware index.js, flows-dir-lising for preview assets link

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -5,11 +5,11 @@ module.exports = async function () {
   subscription.moduleResource.moduleName = 'webbuilder'
   let registerAppModule = {
     'project-configuration': ['create'],
-    'delete-service': ['remove'],
-    'address-book': ['create', 'get', 'find', 'patch'],
-    'request-info': ['create', 'get', 'find'],
-    'myOrders': ['create', 'get', 'find'],
-    'request-quote': ['create', 'get', 'find']
+    'delete-service': ['remove']
+    // 'address-book': ['create', 'get', 'find', 'patch'],
+    // 'request-info': ['create', 'get', 'find'],
+    // 'myOrders': ['create', 'get', 'find'],
+    // 'request-quote': ['create', 'get', 'find']
   }
   subscription.moduleResource.registerAppModule = registerAppModule
   subscription.moduleResource.appRoles = ['admin', 'developer', 'client']
