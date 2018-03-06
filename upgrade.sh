1019cdcc53afc1ca6c0f6048684b4c59e7278cf8
80017 <arpitap@officebeacon.com>
2018-03-06 16:12:00 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -26,6 +26,20 @@ then
       WEBROOTSERVER="$WEBROOTSERVER_DEVELOP";
       SERVERARECORD="$SERVERARECORD_DEVELOP";
   }
+elif [ "$TRAVIS_BRANCH" = "staging" ]
+then
+    {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Staging" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="staging";
+      REGISTER_URL="$REGISTER_URL_STAGING";
+      EMAIL_URL="$EMAIL_URL_STAGING";
+      DOMAINKEY="$DOMAINKEY_STAGING";
+      WEBROOTSERVER="$WEBROOTSERVER_STAGING";
+      SERVERARECORD="$SERVERARECORD_STAGING";
+  }  
 else
   {
       echo "call $TRAVIS_BRANCH branch"
