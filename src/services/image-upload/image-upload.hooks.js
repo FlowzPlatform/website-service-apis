17a0dc11804f112d49194473ec1c3df08788328a
Faizan Saiyed <fsaiyed@officebrain.com>
2018-03-07 10:38:54 +0530

    Favicon image upload fixed. fixed service page

--- src/services/image-upload/image-upload.hooks.js
+++ src/services/image-upload/image-upload.hooks.js
@@ -45,12 +45,22 @@ function before_image_upload(hook) {
 
 function after_image_upload(hook) {
     return new Promise((resolve, reject) => {
-      console.log("Image details:", hook.data);
+      // console.log("Image details:", hook.data);
 
-      fs.writeFile(hook.data.filename, hook.data.text, function(err) {
-          //console.log('test');
-          err ? reject(err) : resolve(hook)
+      // console.log('Hook data: ', hook.data)
+
+      var img = hook.data.text;
+
+      var data = img.replace(/^data:image\/\w+;base64,/, "");
+      var buf = new Buffer(data, 'base64');
+      fs.writeFile(hook.data.filename, buf, function(err) {
+        err ? reject(err) : resolve(hook)
       });
+
+      // fs.writeFile(hook.data.filename, hook.data.text, function(err) {
+      //     //console.log('test');
+      //     err ? reject(err) : resolve(hook)
+      // });
       
     })
 }
