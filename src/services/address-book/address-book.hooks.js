f75f1d48124bf843fc1f86afe0ade0a1502ce7aa
Software <ppriyank@officebrain.com>
2018-04-03 14:10:59 +0530

    shipping and billing default

--- src/services/address-book/address-book.hooks.js
+++ src/services/address-book/address-book.hooks.js
@@ -56,7 +56,7 @@ beforeFind = hook => {
     if(hook.params.query.deleted_at == "false"){
         hook.params.query.deleted_at = false;
     }
-    // console.log("++",hook.params.query);
+
     if(hook.params.query.terms != undefined && hook.params.query.terms !=""){
       hook.params.query.$or = [
         {
@@ -81,32 +81,60 @@ beforeCreateAddressBook = async hook => {
         else if (hook.data.is_address == undefined) {
             throw errors.NotFound(new Error('Address book or contact book detail is missing.'));
         }
-        let is_default = '1';
-        let isDefault = await checkIsDefault(hook);
 
-        if(isDefault.length > 0){
-          is_default = '0';
+        let billing_default = '0';
+        let shipping_default = '0';
+        if(hook.data.address_type.includes("shipping")){
+            shipping_default = '1'
+            let isShipDefault = await checkIsDefault(hook,"shipping");
+            if(isShipDefault.length > 0){
+              shipping_default = '0'
+            }
         }
 
+        if(hook.data.address_type.includes("billing")){  
+            billing_default = '1'
+            let isBillingDefault = await checkIsDefault(hook,"billing");
+            if(isBillingDefault.length > 0){
+              billing_default = '0'
+            }
+        }
+          
         hook.data.created_at = new Date();
         hook.data.updated_at = '';
         hook.data.deleted_at = false;
-        hook.data.is_default = is_default;
+        hook.data.shipping_default = shipping_default;
+        hook.data.billing_default = billing_default;
 }
 
-checkIsDefault = async hook =>{
+// checkIsDefault = async hook =>{
+async function checkIsDefault(hook,address_type){
   return new Promise ((resolve , reject) =>{
-    // console.log(hook.data)
-    r.table('userAddressBook')
-      .filter({'user_id':hook.data.user_id,'is_address':hook.data.is_address,'address_type':hook.data.address_type,'is_default':'1','deleted_at':false,'website_id':hook.data.website_id})
-      .run(connection , function(error , cursor){
-          if (error) throw error;
-          cursor.toArray(function(err, result) {
-              if (err) throw err;
-              resolve(result)
-          });
+    if(address_type == "shipping"){
+        r.table('userAddressBook')
+        // .filter({'user_id':hook.data.user_id,'is_address':hook.data.is_address,'address_type':hook.data.address_type,'is_default':'1','deleted_at':false,'website_id':hook.data.website_id})
+        .filter({'user_id':hook.data.user_id,'is_address':hook.data.is_address, "shipping_default":'1','deleted_at':false,'website_id':hook.data.website_id})
+          .run(connection , function(error , cursor){
+              if (error) throw error;
+              cursor.toArray(function(err, result) {
+                console.log("cursor 123",result)
+                  if (err) throw err;
+                  resolve(result)
+              });
+        })
+      }else{
+          r.table('userAddressBook')
+          .filter({'user_id':hook.data.user_id,'is_address':hook.data.is_address, "billing_default":'1','deleted_at':false,'website_id':hook.data.website_id})
+            .run(connection , function(error , cursor){
+                if (error) throw error;
+                cursor.toArray(function(err, result) {
+                  // console.log("cursor",result)
+                    if (err) throw err;
+                    resolve(result)
+                });
+          })
+      }
     })
-  })
 }
 
 
@@ -116,25 +144,37 @@ beforPatchAddressBook = async hook =>{
         .get(hook.id)
         .run(connection , async function(error , cursor){
            if (error) throw error;
-          //  console.log("cursor.is_default",cursor.is_default);
-           if(hook.data.deleted_at == undefined && cursor.is_default != '1')
+          console.log("cursor.shipping_default",cursor.shipping_default);
+          console.log("cursor.billing_default",cursor.billing_default);
+           if(hook.data.deleted_at == undefined )
            {
-             let obj = '';
-             let user_id = cursor.user_id;
-             let is_address = cursor.is_address;
-             let address_type = cursor.address_type;
-             let website_id = cursor.website_id;
-              obj = {'data':{'user_id': user_id,'is_address': is_address,'address_type': address_type,'website_id':website_id}}
-              let isDefault = await checkIsDefault(obj);
-              // console.log("isdefault",isDefault);
-              if(isDefault.length > 0){
-                  let oldDefaultObj = isDefault[0];
-                  r.table('userAddressBook').get(oldDefaultObj.id).update({'is_default': "0",'updated_at':new Date()}).run(connection)
+              let obj = '';
+              let user_id = cursor.user_id;
+              let is_address = cursor.is_address;
+              let address_type = cursor.address_type;
+              let website_id = cursor.website_id;
+              obj = {'data':{'user_id': user_id,'is_address': is_address,'website_id':website_id}}
+              let isDefault = null
+              
+              if(hook.data.billing_default != undefined ) {
+                  isDefault = await checkIsDefault(obj,"billing");
+                  if(isDefault.length > 0){
+                    let oldDefaultObj = isDefault[0];
+                    r.table('userAddressBook').get(oldDefaultObj.id).update({'billing_default': "0",'updated_at':new Date()}).run(connection)
+                  }
               }
+              if(hook.data.shipping_default != undefined ) {
+                isDefault = await checkIsDefault(obj,"shipping");
+                if(isDefault.length > 0){
+                  let oldDefaultObj = isDefault[0];
+                  r.table('userAddressBook').get(oldDefaultObj.id).update({'shipping_default': "0",'updated_at':new Date()}).run(connection)
+                }
+              }
+              
            }
+
            hook.data.updated_at = new Date();
            resolve(hook)
         })
-        // console.log("id==",hook.id);
     })
 }
