8a8c906b29f02a37532f6d71f4ca931d12722000
Faizan Saiyed <fsaiyed@officebrain.com>
2018-06-07 10:33:12 +0530

    Gitlab api reverted

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ module.exports = {
     path: '/var/www/html/websites/',
     basePath: '/var/www/html/',
     gitLabUrl: 'http://209.50.53.116',
+    // gitLabUrl: 'http://209.50.49.140',
     gitLabUsername: 'fsaiyed',
     mailSendApi: "http://api.flowzcluster.tk/sendmail/email/send",
     // gitLabToken: 'eQ1-V9hyB9PN_XYnYfkV'
