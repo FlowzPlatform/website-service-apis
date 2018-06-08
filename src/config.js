b8cf990039e6559a9f1cce2912b5cf2bc1bce1dd
gkumar9 <gkumar@officebrain.com>
2018-06-08 11:24:54 +0530

    changed gitlab server

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 module.exports = {
     path: '/var/www/html/websites/',
     basePath: '/var/www/html/',
-    gitLabUrl: 'http://209.50.53.116',
+    gitLabUrl: 'http://209.50.50.225',
     // gitLabUrl: 'http://209.50.49.140',
     gitLabUsername: 'fsaiyed',
     mailSendApi: "http://api.flowzcluster.tk/sendmail/email/send",
