b99a7857d534b631dc67eb8b9d9d2cc85c67c551
Software <neelpatel@officebrain.com>
2018-08-27 16:59:25 +0530

    download pdf on compare page

--- plugins/WebsiteTemplates/innovation-line/public/main-files/main.js
+++ plugins/WebsiteTemplates/innovation-line/public/main-files/main.js
@@ -3111,3 +3111,159 @@ $(document).on('click','.js-product-list-btn',function(e) {
         $('.ob-product-grid.js-product-grid').addClass('list-view');
     }
 });
+
+// can move to different js - used in compare page only
+
+$(document).on('click','.js-btn-download-compare-product', async function (e) {
+  showPageAjaxLoading();
+  await sleep(500);
+  let compareHtml = $("#download-comparision");
+  let originalCompareHtml = compareHtml.html();
+  
+  let compareValuesCount = 0;
+  let compare_values='';
+    
+  if(user_details != null)
+  {
+    if(localStorage.getItem("savedComparedRegister") != null && localStorage.getItem("savedComparedRegister").length > 0)
+    {
+      compare_values = JSON.parse(localStorage.getItem("savedComparedRegister"));
+    }
+    else{
+      compare_values = '';        
+    }
+  }
+  else {
+    let decideLocalStorageKey = decide_localStorage_key(3);      
+    compare_values = JSON.parse(localStorage.getItem(decideLocalStorageKey));
+  }
+
+  let itemTitleHtml=productHtml=itemSkuHtml=activeSummaryHtml=itemFeaturesHtml=itemPriceHtml='';
+  let productData;
+  let html = $('#download-comparision #product_img').html();
+  let itemPrice = $('#download-comparision #product_price').html();
+  let item_sku = $('#download-comparision #product_sku').html();
+  let activeSummary = $('#download-comparision #product_summary').html();
+  let item_features = $('#download-comparision #product_features').html();
+
+  if (typeof(compareHtml.html()) !== "undefined" && compare_values != null && compare_values.length > 0) 
+  {
+    for (item in compare_values)
+    {
+        let prodId = compare_values[item]['product_id'];
+
+        $.ajax({
+          type: 'GET',
+          url: project_settings.product_api_url+"?_id="+prodId+"&source=default_image,product_id,sku,product_name,currency,min_price,description,features,images",
+          async: false,
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader ("vid", website_settings.Projectvid.vid);
+          },
+          dataType: 'json',
+          success: async function (data)
+          {
+            productData = data.hits.hits;
+            if(productData.length >0)
+            {
+            compareValuesCount = compareValuesCount+1;
+
+            let itemTitleHtml = html;
+            
+            let product_image = 'https://res.cloudinary.com/flowz/image/upload/v1531481668/websites/images/no-image.png';
+            
+            if( productData[0]._source.images != undefined ){
+              product_image = productData[0]._source.images[0].images[0].secure_url;
+            }
+
+            itemTitleHtml = itemTitleHtml.replace('#data.image#',product_image);
+
+            itemTitleHtml = itemTitleHtml.replace('#data.title#',productData[0]._source.product_name);
+
+            productHtml = itemTitleHtml;
+
+            let itemPriceHtml = itemPrice;
+            if(user_id != null){
+              itemPriceHtml = itemPriceHtml.replace('#data.price#',productData[0]._source.currency+" "+parseFloat(productData[0]._source.min_price).toFixed(project_settings.price_decimal));
+            }
+            let itemSkuHtml1 = item_sku;
+            itemSkuHtml1 = itemSkuHtml1.replace('#data.sku#',productData[0]._source.sku);
+            itemSkuHtml = itemSkuHtml1;
+
+            let itemSummaryHtml = activeSummary;
+            itemSummaryHtml = itemSummaryHtml.replace('#data.summary#',stripHtml(productData[0]._source.description));
+            activeSummaryHtml = itemSummaryHtml;
+
+            let itemFeaturesHtml1 = item_features;
+            
+            let fetureList = '';
+            for (let [i, features] of productData[0]._source.features.entries() ) {
+              fetureList += features.key+": "+features.value+"<br>";
+            }
+
+            itemFeaturesHtml1 = itemFeaturesHtml1.replace('#data.features#',fetureList);
+            itemFeaturesHtml = itemFeaturesHtml1;
+
+            if(item == 0 || compareValuesCount == 1)
+            {
+              compareHtml.find("#product_img").html("<td style='width:20%' class='feature-block'></td>"+productHtml)
+              if(user_id == null)
+              {
+                compareHtml.find("#product_price").remove();
+              }
+              else{
+                compareHtml.find("#product_price").addClass('hide').html("<td style='width:20%' class='feature-block'>PRICE</td>"+itemPriceHtml)
+              }
+              compareHtml.find("#product_sku").html("<td style='width:20%' class='feature-block'>ITEM CODE</td>"+itemSkuHtml)
+              compareHtml.find("#product_summary").html("<td style='width:20%'class='feature-block'>SUMMARY</td>"+activeSummaryHtml)
+              compareHtml.find("#product_features").html("<td style='width:20%' class='feature-block'>FEATURES</td>"+itemFeaturesHtml)
+            }
+            else{
+              compareHtml.find("#product_img").append(productHtml)
+              if(user_id != null)
+              {
+                compareHtml.find("#product_price").append(itemPriceHtml)
+              }
+              compareHtml.find("#product_sku").append(itemSkuHtml)
+              compareHtml.find("#product_summary").append(activeSummaryHtml)
+              compareHtml.find("#product_features").append(itemFeaturesHtml)
+            }
+          }
+          }
+        });
+    }
+
+    axios({
+      method: 'post',
+      url: project_settings.download_pdf_api_url,
+      data: {
+        "html" : '<div id="download-comparision">'+compareHtml.html()+'</div>'
+      },
+      }).then(function (response) {
+        var arrayBufferView = new Uint8Array( response.data.data );
+        var blob=new Blob([arrayBufferView], {type:"application/pdf"});
+        
+        var link=document.createElement('a');
+        link.href=window.URL.createObjectURL(blob);
+        link.download='compare-product';
+        link.click();
+        let compareHtml = $("#download-comparision");
+        $("#download-comparision").html(originalCompareHtml);
+        hidePageAjaxLoading();
+      })
+      .catch(function (error) {
+        hidePageAjaxLoading();
+      })
+      return false;
+  }
+  return false;
+});
+// END - can move to different js - used in compare page only
+
+function stripHtml(html){
+  // Create a new div element
+  var temporalDivElement = document.createElement("div");
+  // Set the HTML content with the providen
+  temporalDivElement.innerHTML = html;
+  // Retrieve the text property of the element (cross-browser support)
+  return temporalDivElement.textContent || temporalDivElement.innerText || "";
+}
