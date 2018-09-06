900c9090f7982ce85ef46e90055599d4af8e8a7b
gkumar9 <gkumar@officebrian.com>
2018-09-06 15:07:32 +0530

    reverted gitlab setting for now

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ module.exports = {
     path: '/var/www/html/websites/',
     basePath: '/var/www/html/',
      // gitLabUrl: 'http://209.50.53.116',
-    // gitLabUrl: 'https://209.50.50.225',
-    gitLabUrl:'https://gitlab.com',
-    // gitLabUrl: 'http://209.50.49.140',
-    gitLabUsername: 'cenacle-devops',
+    gitLabUrl: 'http://209.50.50.225',
+    // gitLabUrl:'https://gitlab.com',
+    // gitLabUrl: 'fsaiyed',
+    gitLabUsername: 'fsaiyed',
     // mailSendApi: "http://api.flowzcluster.tk/sendmail/email/send",
     mailSendApi: "http://api."+process.env.domainKey+"/sendmail/email/send",
     // gitLabToken: 'eQ1-V9hyB9PN_XYnYfkV'
-    gitLabToken: 'r4BStw4h7nhsgdoMnzJP'
+    gitLabToken: 'kR69TpDzkUhqE-u_s-ax'
 }
