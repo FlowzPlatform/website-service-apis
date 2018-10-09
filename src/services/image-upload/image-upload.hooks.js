b86a56881debb32d3f1d8a15003c00ec54462e75
gkumar9 <gkumar@officebrian.com>
2018-10-09 10:16:38 +0530

    favicon code added

--- src/services/image-upload/image-upload.hooks.js
+++ src/services/image-upload/image-upload.hooks.js
@@ -1,6 +1,8 @@
 var dir = require('node-dir');
 const path = require('path');
 let fs = require('fs');
+let axios=require('axios')
+const config = require('../../config');
 
 module.exports = {
   before: {
@@ -44,19 +46,37 @@ function before_image_upload(hook) {
 }
 
 function after_image_upload(hook) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       // console.log("Image details:", hook.data);
 
-      // console.log('Hook data: ', hook.data)
 
-      var img = hook.data.text;
+      let img = hook.data.text;
 
-      var data = img.replace(/^data:image\/\w+;base64,/, "");
-      var buf = new Buffer(data, 'base64');
-      fs.writeFile(hook.data.filename, buf, function(err) {
-        err ? reject(err) : resolve(hook)
-      });
+      let data = img[0].replace(/^data:image\/\w+;base64,/, "");
+      let buf = new Buffer(data, 'base64');
+      let tempjsonpd=''
+      if(img[3]!=-1){
 
+      tempjsonpd='{"action": "update","encoding":"base64","file_path": "favicon.'+img[1]+'","content": "'+data+'" }'
+      }else{
+        tempjsonpd='{"action": "create","encoding":"base64","file_path": "favicon.'+img[1]+'","content": "'+data+'" }'
+      }
+      let buildpayloadpd='{ "branch": "master","commit_message": "uploading favicon", "actions": ['+tempjsonpd+'] }'
+
+      let axiosoptionpd={
+        method:'post',
+        url:'https://gitlab.com/api/v4/projects/'+img[2]+'/repository/commits',
+        data:buildpayloadpd,
+        headers:{ 'PRIVATE-TOKEN':config.gitLabToken, 'Content-Type':'application/json'}
+      }
+
+      await axios(axiosoptionpd)
+      .then(()=>{
+        fs.writeFile(hook.data.filename, buf, function(err) {
+          err ? reject(err) : resolve(hook)
+        });
+        
+      })
       // fs.writeFile(hook.data.filename, hook.data.text, function(err) {
       //     //console.log('test');
       //     err ? reject(err) : resolve(hook)
