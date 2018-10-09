8d6fd14b06ee1606230f0fb3d29c1a8cc5d69545
kdalsania <kdalsania@officebrain.com>
2018-10-09 14:18:42 +0530

    Flyer Mail

--- plugins/WebsiteTemplates/posh/public/assets/client-plugins/client-flyers.js
+++ plugins/WebsiteTemplates/posh/public/assets/client-plugins/client-flyers.js
@@ -41,15 +41,15 @@ async function flyer_data(flyers) {
         await sleep(100);
 
         let listHtml = $('#myFlyers .js-list').html();
-        $.each( flyers, function( key, flyerArray ) {            
+        $.each( flyers, function( key, flyerArray ) {
             let listHtml1 = "";
 
             listHtml1 = listHtml.replace('#flyer-image#',flyerArray.flyer_image);
 
             listHtml1 = listHtml1.replace(/#flyer-id#/g,flyerArray.id);
-            
+
             listHtml1 = listHtml1.replace(/#flyer-title#/g,flyerArray.flyer_name);
-                            
+
             listHtml1 = listHtml1.replace(/#flyer-image-link#/g,flyerArray.flyer_image);
 
             listHtml1 = listHtml1.replace(/#flyer-pdf-link#/g,flyerArray.flyer_pdf);
@@ -64,7 +64,7 @@ async function flyer_data(flyers) {
             }
         });
 
-        
+
         hidePageAjaxLoading();
     }
     else {
@@ -82,3 +82,81 @@ $(document).on("click", '.js-open-modal-flyer', function(e){
     $(".js_add_html").html(flyerHtml);
     $('#modal-table').modal('show');
 });
+
+let flyerImageLink;
+$(document).on("click", '.js-open-flyer-mail', function(e){
+    flyerImageLink = $(this).attr("data-image")
+});
+
+$(document).on('click', '.email-product-modal-submit', function(e) {
+    $('form#email-flyer-form').validate({
+      rules: {
+        "senderName": "required",
+        "fromMail": {
+          required: true,
+          email: true
+        },
+        "to_email": {
+          required: true,
+          email: true
+        },
+        "message": "required"
+      },
+      messages: {
+        "senderName": "Enter Valid Name!",
+        "fromMail": {
+          required : "Enter Email!",
+          email : "Enter Valid Email!"
+        },
+        "to_email": {
+          required : "Enter Email!",
+          email : "Enter Valid Email!"
+        },
+        "message": "Enter Valid Message!"
+      },
+      errorElement: "li",
+      errorPlacement: function(error, element) {
+        error.appendTo(element.closest("div"));
+        $(element).closest('div').find('ul').addClass('red')
+      },
+      errorLabelContainer: "#errors",
+      wrapper: "ul",
+      submitHandler: function(form) {
+        showPageAjaxLoading();
+        let formData = $(form).serializeArray();
+        let flyerJson = {};
+        let mailFlyerJson = {};
+        for (let input of formData) {
+            flyerJson[input.name] = input.value;
+        }
+        flyerJson['slug'] = 'flyer';
+        flyerJson['flyer_image'] = flyerImageLink
+        mailFlyerJson['form_data'] = flyerJson;
+        mailFlyerJson['website_id'] = website_settings['projectID'];
+        console.log('mailFlyerJson',mailFlyerJson)
+
+        $.ajax({
+    		  type : 'POST',
+    		  url : project_settings.request_quote_api_url,
+    		  data : mailFlyerJson,
+    		  cache: false,
+    		  dataType : 'json',
+    		  success : function(response_data) {
+    			console.log("response_data",response_data)
+    			if(response_data!= "") {
+    			//   $("#send_quick_quoteemail").find("input,textarea").val('');
+    				hidePageAjaxLoading()
+    				showSuccessMessage("Email Sent Successfully.");
+    				// window.location = "flyers.html";
+    				return false;
+    			}
+    			else if(response_data.status == 400) {
+    				hidePageAjaxLoading()
+    				showErrorMessage(response_data.message);
+    				return false;
+    			}
+    		  }
+    		});
+      }
+    })
+})
